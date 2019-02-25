a7f779cdaadcb148ee39445560f734a296f166e9
MPDR <mpdr200011@gmail.com>
2019-02-25 14:40:41 +0000

    clear and refresh 2

--- TP2/src/main/java/TP2/App.java
+++ TP2/src/main/java/TP2/App.java
@@ -3,12 +3,33 @@
  */
 package TP2;
 
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+
+            screen.clear();
+            screen.setCharacter(10,10,new TextCharacter('X'));
+            screen.refresh();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
